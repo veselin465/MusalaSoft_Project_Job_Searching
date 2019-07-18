f2bc0770715c1e4a89b9e43e539a4a25d4215d18
veselin <33176441+veselin465@users.noreply.github.com>
2019-07-18 10:31:05 +0300

    Added project's core folders and files

---
+++ JobSearching/JobSearching.Data/JobSearchingDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobSearching.Data
+{
+    class JobSearchingDbContext
+    {
+    }
+}
---
+++ JobSearching/JobSearching.Services/CurrentSignedVolunteer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobSearching.Services
+{
+    public class CurrentSignedVolunteer
+    {
+        public int VolunteerId { get; set; }
+    }
+}
